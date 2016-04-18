c3ae07fe04dc72d9fe0b83d33a679bca1ad4f3a5
Andrei Coman <andrei.coman@iquestgroup.com>
2016-04-18 15:35:05 +0300

    create log file

---
+++ FilleWatchingService/FileWatcher.cs
@@ -0,0 +1,61 @@
+﻿using FilleWatchingService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatchingService
+{
+    public class FileWatcher
+    {
+        private FileSystemWatcher _fileWatcher;
+
+
+        public FileWatcher()
+        {
+            _fileWatcher = new FileSystemWatcher(PathLocation());
+
+            _fileWatcher.Created += new FileSystemEventHandler(_fileWatcher_Created);
+            _fileWatcher.Deleted += new FileSystemEventHandler(_fileWatcher_Deleted);
+            _fileWatcher.Changed += new FileSystemEventHandler(_fileWatcher_Changed);
+
+            _fileWatcher.EnableRaisingEvents = true;
+        }
+
+        private string PathLocation()
+        {
+            string value = String.Empty;
+
+            try
+            {
+                value = System.Configuration.ConfigurationManager.AppSettings["location"];
+            }
+
+            catch (Exception)
+            {
+                //Implement logging on future version.
+            }
+
+
+            return value;
+
+        }
+
+        private void _fileWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(String.Format("File Changed: Path:{0} , Name:{1}", e.FullPath, e.Name));
+        }
+
+        private void _fileWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(String.Format("File Deleted: Path:{0} , Name:{1}", e.FullPath, e.Name));
+        }
+
+        void _fileWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(String.Format("File Created: Path:{0} , Name:{1}", e.FullPath, e.Name));
+        }
+    }
+}
---
+++ FilleWatchingService/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilleWatchingService
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            try
+            {
+                string _message = String.Format("{0} {1}", message, Environment.NewLine);
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "logFile.txt", _message);
+            }
+            catch (Exception)
+            {
+                //Implement logging on next version.
+
+            }
+        }
+    }
+}
