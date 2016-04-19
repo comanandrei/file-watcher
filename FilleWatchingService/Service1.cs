c3fd75749f01c4e7cc2f2b86460739e0dbf7e41a
Andrei Coman <andrei.coman@iquestgroup.com>
2016-04-19 16:41:51 +0300

    not complete - just start to get files

--- FilleWatchingService/Service1.cs
+++ FilleWatchingService/Service1.cs
@@ -9,6 +9,8 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.IO;
+using System.Security.Principal;
 
 namespace FileWatchingService
 {
@@ -29,18 +31,51 @@ namespace FileWatchingService
         protected override void OnStart(string[] args)
         {
             FileWatcher f = new FileWatcher();
-            // Create a timer with a ten second interval.
-            aTimer = new System.Timers.Timer(10000);
+            TestTimer();
+        }
+
+        private void TestTimer()
+        {
+         
+            aTimer = new System.Timers.Timer();
 
             // Hook up the Elapsed event for the timer.
             aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
 
             // Set the Interval to 2 seconds (2000 milliseconds).
-            aTimer.Interval = 2000;
+            aTimer.Interval = 20000;
             aTimer.Enabled = true;
 
+            var pathLocation = PathLocation();
+
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(pathLocation);
+
+            foreach (string fileName in fileEntries)
+            {
+                var user = System.IO.File.GetAccessControl(fileName).GetOwner(typeof(System.Security.Principal.NTAccount)).ToString(); 
+                Console.WriteLine(user); // DOMAIN\username
+            }
 
 
+        }
+
+        private string PathLocation()
+        {
+            string value = String.Empty;
+
+            try
+            {
+                value = System.Configuration.ConfigurationManager.AppSettings["location"];
+            }
+
+            catch (Exception)
+            {
+                //Implement logging on future version.
+            }
+
+
+            return value;
 
         }
 
