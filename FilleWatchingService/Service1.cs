06cafe2a4683932255af5b0383d91f918249476f
Andrei Coman <andrei.coman@iquestgroup.com>
2016-04-19 14:29:09 +0300

    add timer sample for test

--- FilleWatchingService/Service1.cs
+++ FilleWatchingService/Service1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FilleWatchingService;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace FileWatchingService
 {
@@ -22,13 +24,35 @@ namespace FileWatchingService
             OnStart(null);
         }
 
+        private static System.Timers.Timer aTimer;
+
         protected override void OnStart(string[] args)
         {
             FileWatcher f = new FileWatcher();
+            // Create a timer with a ten second interval.
+            aTimer = new System.Timers.Timer(10000);
+
+            // Hook up the Elapsed event for the timer.
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            // Set the Interval to 2 seconds (2000 milliseconds).
+            aTimer.Interval = 2000;
+            aTimer.Enabled = true;
+
+
+
+
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            string message = String.Format("yeeeeeeeeee {0}", DateTime.Now.ToString());
+            Logger.Log(message);
         }
 
         protected override void OnStop()
         {
+            Logger.Log("Stop");
         }
     }
 }
